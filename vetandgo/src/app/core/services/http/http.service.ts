d606ba9407bcbfe940c9aeed9829e21e239b77d0
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-28 18:36:18 +0100

    Automatic Push from Omarchy

--- vetandgo/src/app/core/services/http/http.service.ts
+++ vetandgo/src/app/core/services/http/http.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class Http {
-  private readonly baseUrl: string = '/api';
+  private readonly baseUrl: string = 'http://localhost:8081/api';
 
   constructor(private http: HttpClient) { }
 

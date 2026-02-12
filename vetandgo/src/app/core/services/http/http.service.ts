705a77c89f079c966958bb441574102e1ecd8f6e
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-02-12 12:59:17 +0100

    Automatic Push from Omarchy

--- vetandgo/src/app/core/services/http/http.service.ts
+++ vetandgo/src/app/core/services/http/http.service.ts
@@ -5,7 +5,7 @@ import { environment } from '../../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class Http {
-  private readonly baseUrl: string = 'http://localhost:8080/api';
+  private readonly baseUrl: string = 'http://localhost:8082/api';
 
   constructor(private http: HttpClient) { }
 
